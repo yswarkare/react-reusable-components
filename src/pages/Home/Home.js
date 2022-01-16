d31fe5d24fdd223c7b5a3a27dde9beaba597a961
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-16 20:33:22 +0530

    changed text color

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Home = () => {
+
   return (
     <div id={`home-page`} className={`flex flex-col justify-center content-center items-center`}>
-      <span className='text-12 text-gray-700'>Home</span>
+      <span className='text-12 text-white'>Home  Page</span>
     </div>
   );
 }
