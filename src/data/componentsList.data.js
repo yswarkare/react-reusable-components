a67c4cfff89998f79d6ed19bde90fc2937a092bc
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-16 20:32:13 +0530

    added list of components to render on components page

---
+++ src/data/componentsList.data.js
@@ -0,0 +1,36 @@
+import { checkboxes_link, custom_tabs_link, radio_buttons_link, date_lab_link, tree_view_link } from "../routes/componentRoutes";
+
+const componentsList = [
+  {
+    id: 1,
+    label: 'Radio Buttons',
+    path: `${radio_buttons_link}`,
+    description: ``,
+  },
+  {
+    id: 1,
+    label: 'Checkboxes',
+    path: `${checkboxes_link}`,
+    description: ``,
+  },
+  {
+    id: 1,
+    label: 'Tree View',
+    path: `${tree_view_link}`,
+    description: ``,
+  },
+  {
+    id: 1,
+    label: 'Custom Tabs',
+    path: `${custom_tabs_link}`,
+    description: ``,
+  },
+  {
+    id: 1,
+    label: 'Date Display',
+    path: `${date_lab_link}`,
+    description: ``,
+  },
+]
+
+export default componentsList;
