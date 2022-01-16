af0b18b0eaac2e1cd198217b5fb2c79beb0064c7
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-16 20:31:11 +0530

    created profile component

---
+++ src/pages/Profile/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div id={`home-page`} className={`flex flex-col justify-center content-center items-center`}>
+      <span className='text-12 text-white'>Profile Page</span>
+    </div>
+  );
+}
+
+export default Profile;
