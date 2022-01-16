6b8073127ff6ae880c600c03fbda0c529f717762
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-16 20:33:34 +0530

    changed text color

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
   return (
     <div className={`w-full flex flex-col justify-center content-center items-center`}>
-      <span className={`text-15 text-gray-700`}>About</span>
+      <span className={`text-15 text-white`}>About Page</span>
     </div>
   );
 }
---
+++ src/pages/WhatsNew/WhatsNew.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WhatsNew = () => {
+  return (
+    <div id={`home-page`} className={`flex flex-col justify-center content-center items-center`}>
+      <span className='text-12 text-white'>whats New  Page</span>
+    </div>
+  );
+}
+
+export default WhatsNew;
