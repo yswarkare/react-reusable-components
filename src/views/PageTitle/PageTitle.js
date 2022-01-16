4c5bebad8ba09eb52382915bd388f6b1492923a1
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-16 20:28:27 +0530

    added for page title

---
+++ src/views/PageTitle/PageTitle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageTitle = ({ title }) => {
+  return (
+    <div className={`w-full flex flex-row justify-center content-center items-center`}>
+      <span className={`text-white`}>{title}</span>
+    </div>
+  );
+}
+
+export default PageTitle;
