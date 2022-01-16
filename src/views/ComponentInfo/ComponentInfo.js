73450e83ec6bec851f0e82377bb16c89d305a69e
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-16 20:28:59 +0530

    added for component information

---
+++ src/views/ComponentInfo/ComponentInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ComponentInfo = ({ label, path, description }) => {
+  return (
+    <div className={`w-full px-2 py-1 flex flex-col justify-center content-center items-center`}>
+
+      <Link to={`${path}`} className={`link w-full px-1 py-1/2 min-w-max flex flex-row justify-center content-center items-center`}>
+        {label}
+      </Link>
+      <div className={`w-full px-2 py-1 flex flex-row justify-center content-center items-center`}>
+        <span className='text-white' >{description}</span>
+      </div>
+    </div>
+  );
+}
+
+export default ComponentInfo;
