f8e09f38bf24f0dab7b44d45f8c89483ca39aa53
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-24 10:10:29 +0530

    added sessionApiFuncAction

--- src/redux/sessionStorage/session.actions.js
+++ src/redux/sessionStorage/session.actions.js
@@ -1,4 +1,4 @@
-import { reduxApiAction, reduxStateAction } from "../actions/actions";
+import { reduxApiAction, reduxApiFuncAction, reduxStateAction } from "../actions/actions";
 
 export const sessionStateAction = (method, entity, data) => {
     return reduxStateAction(method, entity, data, "session");
@@ -6,4 +6,8 @@ export const sessionStateAction = (method, entity, data) => {
 
 export const sessionApiAction = (method, url, data, entity) => {
     return reduxApiAction(method, url, data, entity, "session");
+}
+
+export const sessionApiFuncAction = (func, method, data, entity) => {
+    return reduxApiFuncAction(func, method, data, entity, "session");
 }
