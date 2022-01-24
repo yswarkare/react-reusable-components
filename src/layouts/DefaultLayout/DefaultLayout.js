b8d9663308534f6f809bb394683edc5cfbd28254
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-24 10:08:58 +0530

    trial for toast message on api response

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import {
   Outlet,
   useMatch,
@@ -6,6 +7,8 @@ import {
 import Header from '../Header/Header';
 
 const DefaultLayout = () => {
+  const show_api_response_message = useSelector(state => state?.session?.show_api_response_message);
+  const api_response = useSelector(state => state?.session?.api_response);
   const match = useMatch("");
   useEffect(() => {
     if (match) console.log({match});
@@ -13,6 +16,19 @@ const DefaultLayout = () => {
   }, [])
   return (
     <div className={`w-full flex flex-col justify-center content-center items-center`}>
+      {
+        show_api_response_message === true &&
+        <div className={`relative w-full h-0 flex flex-col justify-center content-center items-center`}>
+          <div className={`absolute top-0 right-0 z-50 ionic-material-card w-fit flex flex-col justify-center content-center items-center`}>
+            <span className='text-white'>
+              {api_response?.status}
+            </span>
+            <span className='text-white'>
+              {api_response?.message}
+            </span>
+          </div>
+        </div>
+      }
       <header className={`w-full flex flex-col justify-center content-center items-center`} >
         <Header></Header>
       </header>
