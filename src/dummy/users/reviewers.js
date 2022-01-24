e89084e1438ecc8858cdcdb187e2dbd71eb2901d
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-24 10:06:38 +0530

    added dummy data

---
+++ src/dummy/users/reviewers.js
@@ -0,0 +1,88 @@
+const reviewers = [
+	{
+		id: '1',
+		name: 'Patrick Jane',
+		email: 'patrickjane@gmail.com',
+		reviewerRole: 'Provider',
+		frequency: 'Bi-Weekly',
+	},
+	{
+		id: '2',
+		name: 'Gale Burtrum',
+		email: 'galebertram@gmail.com',
+		reviewerRole: 'Organizer',
+		frequency: 'Daily',
+	},
+	{
+		id: '3',
+		name: 'Bob Kirkland',
+		email: 'bobkirkland@gmail.com',
+		reviewerRole: 'Supplier',
+		frequency: 'Monthly',
+	},
+	{
+		id: '4',
+		name: 'Thomas McAllister',
+		email: 'tommcallister@gmail.com',
+		reviewerRole: 'Developer',
+		frequency: 'Bi-Monthly',
+	},
+	{
+		id: '5',
+		name: 'Reede Smith',
+		email: 'reedesmith@gmail.com',
+		reviewerRole: 'Provider',
+		frequency: 'Weekly',
+	},
+	{
+		id: '6',
+		name: 'Ray Haffner',
+		email: 'rayhaffner@gmail.com',
+		reviewerRole: 'Supplier',
+		frequency: 'Monthly',
+	},
+	{
+		id: '7',
+		name: 'Brad Partridge',
+		email: 'bradpartridge@gmail.com',
+		reviewerRole: 'Supplier',
+		frequency: 'Monthly',
+	},
+	{
+		id: '8',
+		name: 'Patrick Jane',
+		email: 'patrickjane@gmail.com',
+		reviewerRole: 'Provider',
+		frequency: 'Bi-Weekly',
+	},
+	{
+		id: '9',
+		name: 'Gale Burtrum',
+		email: 'galebertram@gmail.com',
+		reviewerRole: 'Organizer',
+		frequency: 'Daily',
+	},
+	{
+		id: '10',
+		name: 'Thomas McAllister',
+		email: 'tommcallister@gmail.com',
+		reviewerRole: 'Developer',
+		frequency: 'Bi-Monthly',
+	},
+	{
+		id: '11',
+		name: 'Reede Smith',
+		email: 'reedesmith@gmail.com',
+		reviewerRole: 'Provider',
+		frequency: 'Weekly',
+	},
+	{
+		id: '12',
+		name: 'Ray Haffner',
+		email: 'rayhaffner@gmail.com',
+		reviewerRole: 'Supplier',
+		frequency: 'Monthly',
+	},
+];
+
+export default reviewers;
