5d5f0ea8e6e8fcf1e6e0e4290a7e894b7ee3adf6
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-24 10:09:40 +0530

    added function for api call

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,19 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const reduxStateAction =
-	(method, entity, data, storage = "session") => async (dispatch) => {
-		console.log({method, entity, data, storage})
+	(method, entity, data, storage = 'session') =>
+	async (dispatch) => {
+		console.log({ method, entity, data, storage });
 		let type = ``;
 		if (Array.isArray(entity) && Array.isArray(data)) {
-			if (entity?.length !== data?.length) throw console.log("entity length is not equal to data length");
-			type = `array_${(method)}_[${(entity.toString())}]_local`;
+			if (entity?.length !== data?.length)
+				throw console.log('entity length is not equal to data length');
+			type = `array_${(method)}_[${entity.toString()}]_local`;
 		}
-		if (typeof(entity) === "string") {
+		if (typeof entity === 'string') {
 			type = `${(method)}_${(entity)}_${(storage)}`;
 		}
 		try {
 			await dispatch({
-				type: (type),
+				type: type,
 				payload: data,
 				method,
 				entity,
@@ -26,8 +28,8 @@ export const reduxStateAction =
 		}
 	};
 
-export const reduxApiAction =	
-	(method, url, data, entity, storage = "session") =>
+export const reduxApiAction =
+	(method, url, data, entity, storage = 'session') =>
 	async (dispatch) => {
 		try {
 			let res = await axios({
@@ -44,6 +46,56 @@ export const reduxApiAction =
 					entity,
 				});
 			}
+		} catch (error) {
+			console.log(error);
+			console.log({ error });
+		}
+	};
+
+export const reduxApiFuncAction =
+	(func, method, data, entity, storage = 'session') =>
+	async (dispatch) => {
+		try {
+			await dispatch({
+				type: `wating_to_${(method)}_${(entity)}_${(storage)}`,
+				payload: true,
+				method,
+				entity,
+			});
+			let res = await func({ data: data });
+			console.log({res});
+			if (res?.status === 200 || res?.status === 201) {
+				await dispatch({
+					type: `${(method)}_${(entity)}_${(storage)}`,
+					payload: res?.data,
+					method,
+					entity,
+				});
+			}
+			await dispatch({
+				type: `show_api_response_message_${storage}`,
+				payload: res?.data,
+				message: res?.message,
+				status: res?.status,
+				method,
+				entity,
+			});
+			await dispatch({
+				type: `wating_to_${(method)}_${(entity)}_${(storage)}`,
+				payload: false,
+				method,
+				entity,
+			});
+			await setTimeout(async ()=>{
+				await dispatch({
+					type: `remove_api_response_message_${storage}`,
+					payload: res?.data,
+					message: res?.message,
+					status: res?.status,
+					method,
+					entity,
+				});
+			}, 5000);
 		} catch (error) {
 			console.log(error);
 			console.log({ error });
