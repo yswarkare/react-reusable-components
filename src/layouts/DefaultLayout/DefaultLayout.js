59b53377c9c38c34d78638e8a6ec96d72b1e0454
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-25 21:59:10 +0530

    updated according to new changes

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -7,8 +7,8 @@ import {
 import Header from '../Header/Header';
 
 const DefaultLayout = () => {
-  const show_api_response_message = useSelector(state => state?.session?.show_api_response_message);
-  const api_response = useSelector(state => state?.session?.api_response);
+  const show_api_response_get_users = useSelector(state => state?.session?.show_api_response_get_users);
+  const api_response_get_users = useSelector(state => state?.session?.api_response_get_users);
   const match = useMatch("");
   useEffect(() => {
     if (match) console.log({match});
@@ -17,14 +17,14 @@ const DefaultLayout = () => {
   return (
     <div className={`w-full flex flex-col justify-center content-center items-center`}>
       {
-        show_api_response_message === true &&
+        show_api_response_get_users === true &&
         <div className={`relative w-full h-0 flex flex-col justify-center content-center items-center`}>
           <div className={`absolute top-0 right-0 z-50 ionic-material-card w-fit flex flex-col justify-center content-center items-center`}>
             <span className='text-white'>
-              {api_response?.status}
+              {api_response_get_users?.status}
             </span>
             <span className='text-white'>
-              {api_response?.message}
+              {api_response_get_users?.message}
             </span>
           </div>
         </div>
