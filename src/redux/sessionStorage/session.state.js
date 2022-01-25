6b1cecdbd5bbdf2dfc34412dc3641ee287fade79
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-01-25 21:57:44 +0530

    removed unnecessary objects

--- src/redux/sessionStorage/session.state.js
+++ src/redux/sessionStorage/session.state.js
@@ -2,11 +2,6 @@ import users from "../../dummy/users/users";
 
 const sessionState = {
   users: users,
-  show_api_response_message: false,
-  api_response: {
-    message: "",
-    status: "",
-  }
 }
 
 export default sessionState;
