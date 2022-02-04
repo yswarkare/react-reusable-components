d90f118a42b652b7e21b378ae570fa69b0c6a38d
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-02-04 21:17:22 +0530

    tested custom hooks for local and session storage

--- src/pages/Hooks/Hooks.js
+++ src/pages/Hooks/Hooks.js
@@ -1,7 +1,26 @@
+import useSessionStorage from '../../hooks/useSessionStorage';
+
 const Hooks = () => {
+	const [name, setName] = useSessionStorage('name');
+	
+
 	return (
-		<div className={`w-full flex flex-col justify-center content-center items-center`}>
+		<div className={`w-full gap-2 flex flex-col justify-center content-center items-center`}>
 			<span className={`text-15 text-white`}>React Hooks</span>
+			<div className={`w-full flex flex-col justify-center content-center items-center`}>
+				<input
+					type={`text`}
+					className={`w-1/3 px-2 py-1 bg-gray-900 active:bg-gray-800 focus:bg-gray-700`}
+					name={`name`}
+					value={name}
+					onChange={(e) => {
+						setName(e.target.value);
+					}}
+				></input>
+			</div>
+			<div className={`w-full flex flex-col justify-center content-center items-center`}>
+				<spans>{name}</spans>
+			</div>
 		</div>
 	);
 };
