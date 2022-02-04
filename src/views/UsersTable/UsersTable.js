870d39b8dacadd02c0296cb52c236d3cfe429ce3
Y. S. Warkare <52399544+yswarkare@users.noreply.github.com>
2022-02-04 21:18:37 +0530

    used input cell component to render editable cell

--- src/views/UsersTable/UsersTable.js
+++ src/views/UsersTable/UsersTable.js
@@ -1,17 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import updateNestedObjecct from '../../utils/updateNestedObject';
-// import updateNestedSessionObject from '../../redux/sessionStorage/actions/updateNestedSessionObject';
+import InputCell from './InputCell';
 
 const UsersTable = () => {
 	const dispatch = useDispatch();
 	const users = useSelector((state) => state?.session?.users);
-	const [editedItems, setEditedItems] = useState({});
+	const [editedItems, setEditedItems] = useState(() => ({}));
+
+	useEffect(() => {
+		console.log(editedItems);
+	}, [editedItems]);
+
+	const updateEditedItems = (e, user) => {
+		setEditedItems((prevEdited) => ({
+			...prevEdited,
+			[user.id]: {
+				...prevEdited[user.id],
+				[e.target.name]: e.target.value,
+			},
+		}));
+	};
 
 	const addToEditedItems = (item) => {
 		setEditedItems((prevEdited) => {
 			return {
-				prevEdited,
+				...prevEdited,
 				[item.id]: item,
 			};
 		});
@@ -25,9 +38,7 @@ const UsersTable = () => {
 		});
 	};
 
-	const deleteUser = (user) => {
-
-	};
+	const deleteUser = (user) => {};
 
 	const columnClasses = `w-25/100`;
 
@@ -50,18 +61,48 @@ const UsersTable = () => {
 						>
 							<td
 								className={`${columnClasses} h-full flex flex-col justify-start content-start items-start`}
-							> 
-								{user?.name}
+							>
+								<InputCell
+									type={`text`}
+									name={'name'}
+									className={`${editedItems[user.id] ? 'bg-gray-900' : ''}`}
+									label={`${user?.name}`}
+									value={editedItems[user.id] ? editedItems[user.id]['name'] : user?.name}
+									edit={editedItems[user.id]}
+									onChange={(e) => {
+										updateEditedItems(e, user);
+									}}
+								></InputCell>
 							</td>
 							<td
 								className={`${columnClasses} h-full flex flex-col justify-start content-start items-start`}
 							>
-								{user?.username}
+								<InputCell
+									type={`text`}
+									name={'username'}
+									className={`${editedItems[user.id] ? 'bg-gray-900' : ''}`}
+									label={`${user?.username}`}
+									value={editedItems[user.id] ? editedItems[user.id]['username'] : user?.username}
+									edit={editedItems[user.id]}
+									onChange={(e) => {
+										updateEditedItems(e, user);
+									}}
+								></InputCell>
 							</td>
 							<td
 								className={`${columnClasses} h-full flex flex-col justify-start content-start items-start`}
 							>
-								{user?.email}
+								<InputCell
+									type={`text`}
+									name={'email'}
+									className={`${editedItems[user.id] ? 'bg-gray-900' : ''}`}
+									label={`${user?.email}`}
+									value={editedItems[user.id] ? editedItems[user.id]['email'] : user?.email}
+									edit={editedItems[user.id]}
+									onChange={(e) => {
+										updateEditedItems(e, user);
+									}}
+								></InputCell>
 							</td>
 							<td
 								className={`${columnClasses} h-full flex flex-row flex-grow justify-evenly content-center items-center`}
@@ -70,13 +111,17 @@ const UsersTable = () => {
 									<>
 										<div
 											className={`w-fit p-1 cursor-pointer transition translate duration-300 flex flex-col justify-center content-center items-center`}
-                      onClick={()=>{removeEditedItem(user)}}
+											onClick={() => {
+												removeEditedItem(user);
+											}}
 										>
 											<i className={`fas fa-save text-white text-base`}></i>
 										</div>
 										<div
 											className={`w-fit p-1 cursor-pointer transition translate duration-300 flex flex-col justify-center content-center items-center`}
-                      onClick={()=>{removeEditedItem(user)}}
+											onClick={() => {
+												removeEditedItem(user);
+											}}
 										>
 											<i className={`far fa-times-circle text-white text-base`}></i>
 										</div>
@@ -84,7 +129,9 @@ const UsersTable = () => {
 								) : (
 									<div
 										className={`w-fit p-1 cursor-pointer transition translate duration-300 flex flex-col justify-center content-center items-center`}
-                    onClick={()=>{addToEditedItems(user)}}
+										onClick={() => {
+											addToEditedItems(user);
+										}}
 									>
 										<i className={`fas fa-pen text-white text-base`}></i>
 									</div>
